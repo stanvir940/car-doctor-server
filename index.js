88cdc3bd846c9aeed0eaef1e729fcc950b5ef761
stanvir940 <hmtanvir940@gmail.com>
2024-09-27 23:04:54 +0600

    done

--- index.js
+++ index.js
@@ -31,6 +31,32 @@ const client = new MongoClient(uri, {
   },
 });
 
+// middlewares
+const logger = async (req, res, next) => {
+  console.log("called : ", req.host, req.originalUrl);
+  next();
+};
+
+const verifyToken = async (req, res, next) => {
+  const token = req.cookies?.token;
+  console.log("value of token is middleware ", token);
+  if (!token) {
+    return res.status(401).send({ message: "not authorized" });
+  }
+
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+    // error
+    if (err) {
+      console.log("error : ", err);
+      return res.status(401).send({ message: "unauthorized" });
+    }
+    // if token if valid then it would be decoded
+    console.log("value in the token ", decoded);
+    req.user = decoded;
+    next();
+  });
+};
+
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
@@ -40,7 +66,7 @@ async function run() {
     const bookingsCollection = client.db("service").collection("bookings");
 
     // auth related api
-    app.post("/jwt", async (req, res) => {
+    app.post("/jwt", logger, async (req, res) => {
       const user = req.body;
       console.log(user);
       const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
@@ -55,7 +81,7 @@ async function run() {
     });
 
     // services related api
-    app.get("/services", async (req, res) => {
+    app.get("/services", logger, async (req, res) => {
       const cursor = serviceCollection.find();
       const result = await cursor.toArray();
       res.send(result);
@@ -74,9 +100,15 @@ async function run() {
     });
 
     // bookings
-    app.get("/bookings", async (req, res) => {
+    app.get("/bookings", logger, verifyToken, async (req, res) => {
       console.log(req.query.email);
+      console.log("user from valid token", req.user);
+
+      if (req.query.email !== req.user.email) {
+        return res.status(403).send({ message: "forbidden access" });
+      }
       let query = {};
+      // console.log("tok token: ", req.cookies.token);
       if (req.query?.email) {
         query = { email: req.query.email };
       }
@@ -87,7 +119,6 @@ async function run() {
     app.post("/bookings", async (req, res) => {
       const bookings = req.body;
       console.log(bookings);
-      console.log("token ", req.cookies.token);
       const result = await bookingsCollection.insertOne(bookings);
       res.send(result);
     });
